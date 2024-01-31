5e54f7cc7b439a2d9c664a81b88039ec304fae65
Shubham Adhikary <shubhamadhk15@gmail.com>
2024-01-31 16:59:28 +0530

    feat(R-105372): Implemented Basic File Structure (#5)
    * Basic Prototype Backbone
    
    * Basic Filesystem
    
    * Deleting Redundant Files
    
    * Connection to PostGre Database Phase 1
    
    * File Structure Updations
    
    * Delete .settings directory
    
    * Minor controller changes
    
    * feat(R-102380): added github workflow (#3) (#6)
    
    * feat: added github workflow
    
    * update action version
    
    Co-authored-by: sutirthak <65647135+sutirthak@users.noreply.github.com>
    
    * Fixes
    
    * Deleted redundant files
    
    * Moved ServiceIMPL to the same Service Package
    
    * Updated the version number and removed target folder
    
    * updating .gitignore
    
    * deleting redundant packages
    
    * Updating Readme.md
    
    * Updating Readme.md
    
    * Updating Readme.md
    
    * removed doc folder
    
    ---------
    
    Co-authored-by: sutirthak <65647135+sutirthak@users.noreply.github.com>
    Co-authored-by: sutirthak <sutirtha.kayal@quantuminventions.com>

---
+++ src/main/java/com/qinvent/hr/service/EmployeeServ.java
@@ -0,0 +1,11 @@
+package com.qinvent.hr.service;
+import java.util.List;
+import java.util.Optional;
+import com.qinvent.hr.models.Employee;
+public interface EmployeeServ {
+	public Employee addEmployee(Employee employee);
+	public String removeEmployee(int id);
+	public Optional<Employee> findEmployee(int id);
+	public List<Employee> getAllEmployee();
+	public String updateEmployee(int id);
+}
