1de5f8a4af4fe583453c597c2b3cd426105696b0
Ayan Sarkar <71203077+Ayan-10@users.noreply.github.com>
2024-01-31 18:52:48 +0530

    feat(R-105411): Implemented Build Workflow (#7)
    * Added workflows
    
    ---------
    
    Co-authored-by: Shubham Adhikary <shubhamadhk15@gmail.com>

--- src/test/java/com/projects/ApplicationTests.java
+++ src/test/java/com/projects/ApplicationTests.java
@@ -1,13 +1,10 @@
 package com.projects;
 
+import com.qinvent.hr.Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmployeeManagementSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class ApplicationTests {
 
 }
