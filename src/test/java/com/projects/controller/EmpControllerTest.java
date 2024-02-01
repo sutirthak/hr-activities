0ebe0f92b83fff316e7d9abbab9b3594c2d68483
Ayan Sarkar <71203077+Ayan-10@users.noreply.github.com>
2024-02-01 12:14:39 +0530

    feat: Added tests for controller

---
+++ src/test/java/com/projects/controller/EmpControllerTest.java
@@ -0,0 +1,23 @@
+package com.projects.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.qinvent.hr.controller.EmpController;
+
+/**
+ * Test class for EmpController
+ */
+@SpringBootTest(classes = {EmpController.class})
+class EmpControllerTest {
+
+	@Autowired
+	private EmpController controller;
+
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+}
