4227480c8d681627f456874940225999e1a05742
Ayan Sarkar <71203077+Ayan-10@users.noreply.github.com>
2024-02-02 18:28:10 +0530

    Added all API endpoints to connect with database

--- src/test/java/com/projects/controller/EmpControllerTest.java
+++ src/test/java/com/projects/controller/EmpControllerTest.java
@@ -1,7 +1,7 @@
 package com.projects.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
