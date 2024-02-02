1cbcb95063c5be62e9e38e52f1dd809fc1447901
Ayan Sarkar <71203077+Ayan-10@users.noreply.github.com>
2024-02-02 15:00:07 +0530

    feat: Added tests for service

--- src/test/java/com/projects/service/EmployeeServIMPLTest.java
+++ src/test/java/com/projects/service/EmployeeServIMPLTest.java
@@ -1,11 +1,16 @@
 package com.projects.service;
 
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.qinvent.hr.models.Employee;
 import com.qinvent.hr.service.EmployeeServIMPL;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +29,16 @@ public class EmployeeServIMPLTest {
     public void shouldReturnAllEmployees() {
         List<Employee> expected = employeeservice.getAllEmployee();
         assertFalse(expected.isEmpty());
+    }
+
+    @Test
+    public void whenAddEmployee_shouldReturnEmployee() {
+        Employee user = new Employee();
+        user.setName("Test Name");
+        when(employeeservice.addEmployee(ArgumentMatchers.any(Employee.class))).thenReturn(user);
+        Employee created = employeeservice.addEmployee(user);
+        assertThat(created.getName()).isSameAs(user.getName());
+        verify(employeeservice).addEmployee(user);
     }
 
 }
